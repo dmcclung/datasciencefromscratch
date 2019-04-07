18a19274cb1d54365658a0622353f94903306d17
Dylan McClung <dylan.mcclung@gmail.com>
2019-04-07 00:46:51 +0000

    More work on chapter 1

--- Chapter1Introduction.py
+++ Chapter1Introduction.py
@@ -1,15 +1,19 @@
 users = [
-    {"id": 0, "name": "Teagan"},
-    {"id": 1, "name": "Declan"},
-    {"id": 2, "name": "Carl"},
-    {"id": 3, "name": "Don"},
-    {"id": 4, "name": "Susan"},
-    {"id": 5, "name": "Liz"},
-    {"id": 6, "name": "Bobby"}
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+    {"id": 3, "name": "Chi"},
+    {"id": 4, "name": "Thor"},
+    {"id": 5, "name": "Clive"},
+    {"id": 6, "name": "Hicks"},
+    {"id": 7, "name": "Devin"},
+    {"id": 8, "name": "Kate"},
+    {"id": 9, "name": "Klein"}
 ]
 
 friendships = [
-    (0, 1), (0, 2), (0, 3), (0, 4), (4, 5), (4, 6)
+    (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)
 ]
 
 for user in users:
@@ -21,6 +25,7 @@ for i, j in friendships:
 
 
 def number_of_friends(user):
+    """Return user number of friends"""
     return len(user["friends"])
 
 
@@ -29,3 +34,17 @@ num_users = len(users)
 avg_connections = total_connections / num_users
 
 print(avg_connections)
+
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+print(num_friends_by_id)
+
+sorted_num_friends_by_id = sorted(num_friends_by_id, key=lambda num_friends: num_friends[1], reverse=True)
+
+print(sorted_num_friends_by_id)
+
+def friends_of_friends_ids_bad(user):
+    return [foaf["id"] for friend in user["friends"]
+            for foaf in friend["friends"]]
+
+print(friends_of_friends_ids_bad(users[0]))
+
